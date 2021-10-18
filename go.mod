d868ae60ad4efd44404d0cfb08fec3319e6533f7
chenyu <chenyu721@foxmail.com>
2021-10-18 15:33:29 +0800

    测试kafka成功

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/chenyu/kafka-test
+
+go 1.16
+
+require github.com/Shopify/sarama v1.30.0
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Shopify/sarama"
+)
+
+func producer_test() {
+	fmt.Printf("producer_test\n")
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+	config.Version = sarama.V0_11_0_2
+
+	producer, err := sarama.NewAsyncProducer([]string{"192.168.210.199:9092"}, config)
+	if err != nil {
+		fmt.Printf("producer_test create producer error :%s\n", err.Error())
+		return
+	}
+
+	defer producer.AsyncClose()
+
+	// send message
+	msg := &sarama.ProducerMessage{
+		Topic: "kafka_go_test",
+		Key:   sarama.StringEncoder("go_test"),
+	}
+
+	value := "this is message"
+	for {
+		fmt.Scanln(&value)
+		msg.Value = sarama.ByteEncoder(value)
+		fmt.Printf("input [%s]\n", value)
+
+		// send to chain
+		producer.Input() <- msg
+
+		select {
+		case suc := <-producer.Successes():
+			fmt.Printf("offset: %d,  timestamp: %s", suc.Offset, suc.Timestamp.String())
+		case fail := <-producer.Errors():
+			fmt.Printf("err: %s\n", fail.Err.Error())
+		}
+	}
+}
+
+func consumer_test() {
+	fmt.Printf("consumer_test")
+
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Version = sarama.V0_11_0_2
+
+	// consumer
+	consumer, err := sarama.NewConsumer([]string{"192.168.210.199:111"}, config)
+	if err != nil {
+		fmt.Printf("consumer_test create consumer error %s\n", err.Error())
+		return
+	}
+
+	defer consumer.Close()
+
+	partition_consumer, err := consumer.ConsumePartition("kafka_go_test", 0, sarama.OffsetOldest)
+	if err != nil {
+		fmt.Printf("try create partition_consumer error %s\n", err.Error())
+		return
+	}
+	defer partition_consumer.Close()
+
+	for {
+		select {
+		case msg := <-partition_consumer.Messages():
+			fmt.Printf("msg offset: %d, partition: %d, timestamp: %s, value: %s\n",
+				msg.Offset, msg.Partition, msg.Timestamp.String(), string(msg.Value))
+		case err := <-partition_consumer.Errors():
+			fmt.Printf("err :%s\n", err.Error())
+		}
+	}
+
+}
+
+func main() {
+	fmt.Println("hello world")
+
+	go producer_test()
+	go consumer_test()
+	select {}
+}
